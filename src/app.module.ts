28753ba1ca8c3d209f196680cda1b186f5b9dbce
alexAskerko <askerko50@gmail.com>
2023-02-27 22:13:45 +0300

    config nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://askerko50:64vzJmdTGUSyVKiL@cluster0.byzwxqv.mongodb.net/?retryWrites=true&w=majority'),
+    ConfigModule.forRoot({
+      envFilePath:`.${process.env.NODE_ENV}.env`
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
